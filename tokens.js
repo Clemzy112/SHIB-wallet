6eaf41474d2f3874935d4ccbbc198fe1024e9db2
Clemzy112 <clementbtc2023@gmail.com>
2026-05-21 20:57:11 +0100

    Add token data for Ethereum, USDT, and Shiba Inu

---
+++ tokens.js
@@ -0,0 +1,43 @@
+const TOKENS = [
+
+{
+name: "Ethereum",
+symbol: "ETH",
+type: "native",
+
+logo:
+"https://cryptologos.cc/logos/ethereum-eth-logo.png",
+
+chart:
+"https://dexscreener.com/ethereum/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2?embed=1"
+},
+
+{
+name: "Tether USD",
+symbol: "USDT",
+
+address:
+"0xdAC17F958D2ee523a2206206994597C13D831ec7",
+
+logo:
+"https://cryptologos.cc/logos/tether-usdt-logo.png",
+
+chart:
+"https://dexscreener.com/ethereum/0xdac17f958d2ee523a2206206994597c13d831ec7?embed=1"
+},
+
+{
+name: "Shiba Inu",
+symbol: "SHIB",
+
+address:
+"0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE",
+
+logo:
+"https://cryptologos.cc/logos/shiba-inu-shib-logo.png",
+
+chart:
+"https://dexscreener.com/ethereum/0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE?embed=1"
+}
+
+];
